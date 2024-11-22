d718c8440cd5c3ccd35c1c2dec52c30a3b22e249
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 17:49:06 +0530

    Added the attributes for the Booking class

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Booking.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Booking.java
@@ -1,4 +1,27 @@
 package com.example.RideBookingBackend.Models;
 
-public class Booking {
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Booking extends BaseModel {
+
+    private Date startTime;
+
+    private Date endTime;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
 }
