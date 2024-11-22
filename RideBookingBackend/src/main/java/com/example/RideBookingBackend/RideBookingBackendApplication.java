dfc790dd131aa2ee9c3d015f677da21c289ca2cc
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 14:50:51 +0530

    Intial commit

---
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/RideBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.RideBookingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBookingBackendApplication.class, args);
+	}
+
+}
