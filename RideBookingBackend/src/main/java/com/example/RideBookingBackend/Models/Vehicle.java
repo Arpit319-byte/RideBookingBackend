d5d8f30f3981cd6a13bb11e6285305445081a2d2
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 21:53:06 +0530

    Added the attributes for the vehicle class

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Vehicle.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Vehicle.java
@@ -1,4 +1,34 @@
 package com.example.RideBookingBackend.Models;
 
-public class Vehicle {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Vehicle extends BaseModel{
+
+    @Column(nullable = false,unique = true)
+    private String vehicleNumber;
+
+    @Column(nullable = false)
+    private String vehicleModel;
+
+    @Column(nullable = false)
+    private String vehicleType;
+
+    @OneToOne(mappedBy = "vehicle")
+    private Driver driver;
+
+
 }
