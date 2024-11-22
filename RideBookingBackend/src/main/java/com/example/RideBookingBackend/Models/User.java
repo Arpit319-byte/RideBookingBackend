5600fe6f845ce06bd83b7fe909012dd3ff88bb88
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 21:58:37 +0530

    Added the attributes for the User Class

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/User.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/User.java
@@ -1,4 +1,31 @@
 package com.example.RideBookingBackend.Models;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User extends BaseModel{
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false,unique = true)
+    private String userContact;
+
+    @Column(nullable = false,unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String userAddress;
+
+
 }
