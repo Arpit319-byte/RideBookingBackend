0f293e479d0cf6c394c0114564c00f817fb35125
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 15:01:16 +0530

    Added the BaseModel class

---
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/BaseModel.java
@@ -0,0 +1,34 @@
+package com.example.RideBookingBackend.Models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    protected Date created_at;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    protected Date updated_at;
+
+}
