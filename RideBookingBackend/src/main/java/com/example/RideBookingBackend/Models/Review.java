c4d0707bd4f10b0090430305bc2bd8679bc15735
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 18:19:44 +0530

    Added the attributes for Review class

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Review.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Review.java
@@ -1,4 +1,18 @@
 package com.example.RideBookingBackend.Models;
 
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Review {
+
+    @Column(nullable = false)
+    private String comment;
+
+    private Double rating;
 }
