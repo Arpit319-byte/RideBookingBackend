59beb87d160a184fb74af761ef5b148263682be0
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 17:52:53 +0530

    Added the Attributes for the BookingStatus enum

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/BookingStatus.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/BookingStatus.java
@@ -1,4 +1,11 @@
 package com.example.RideBookingBackend.Models;
 
 public enum BookingStatus {
+
+    SCHEDULED,
+    CANCELLED,
+    CAB_ARRIVED,
+    ASSIGNING_DRIVER,
+    IN_RIDE,
+    COMPLETED
 }
