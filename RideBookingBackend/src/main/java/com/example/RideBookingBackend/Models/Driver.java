9cccaa4575cab9232320bf1b158c63b591c43a37
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-22 21:44:44 +0530

    Added the attributes of the Driver class

--- RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Driver.java
+++ RideBookingBackend/src/main/java/com/example/RideBookingBackend/Models/Driver.java
@@ -1,4 +1,30 @@
 package com.example.RideBookingBackend.Models;
 
-public class Driver {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Driver extends BaseModel {
+
+    @Column(nullable = false)
+    private String driverName;
+
+    @Column(nullable = false)
+    private String driverContact;
+
+    @Column(nullable = false)
+    private String driverEmail;
+
+    @Column(nullable = false)
+    private String driverLicense;
+
+    @Column(nullable = false)
+    private String driverAddress;
+
 }
